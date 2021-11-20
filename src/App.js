b2b820b4654c6c09e33f3f4db7140d368bd58829
joecaro <jsc11598@gmail.com>
2021-11-20 02:20:38 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Example from "./components/Example";
+
+const html = `<div class='container'><div class='box'></div></div>`;
+const css = `
+.container {
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  place-items: center;
+  background-color: #999;
+}
+
+.box {
+  width: 10px;
+  height: 10px;
+  background-color: black;
+}
+`;
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <Example Html={html} Css={css} />
+      <Example Html={html} Css={css} />
     </div>
   );
 }
--- src/components/Example.js
+++ src/components/Example.js
@@ -1,5 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import { Controlled as CodeMirror } from "react-codemirror2";
 
-export default function Example() {
-  return <section></section>;
+export default function Example({ Html, Css }) {
+  const [selectedTab, setSelectedTab] = useState("css");
+  const [css, setCss] = useState(Css);
+  const [html, setHtml] = useState(Html);
+
+  const srcDoc = `
+	<html>
+		<body>${html}</body>
+		<style>${css}</style>
+	</html>
+  `;
+
+  const handleCssChange = (editor, data, value) => {
+    setCss(value);
+  };
+  const handleHtmlChange = (editor, data, value) => {
+    setHtml(value);
+  };
+  return (
+    <section>
+      <div class='section-container'>
+        <div className='title-container'>
+          <h2 className='title'>title</h2>
+          <div className='support'>details</div>
+        </div>
+        <div className='ex-container'>
+          <div className='example'>
+            <iframe
+              srcDoc={srcDoc}
+              title='example'
+              width='100%'
+              height='100%'
+              frameBorder='0'></iframe>
+          </div>
+          <div className='code-container'>
+            <div className='tab-container'>
+              <button
+                style={{
+                  backgroundColor: `${
+                    selectedTab === "html" ? "#263238" : "#e4e4e4"
+                  }`,
+                }}
+                onClick={() => setSelectedTab("html")}>
+                html
+              </button>
+              <button
+                style={{
+                  backgroundColor: `${
+                    selectedTab === "css" ? "#263238" : "#e4e4e4"
+                  }`,
+                }}
+                onClick={() => setSelectedTab("css")}>
+                css
+              </button>
+            </div>
+            {selectedTab === "html" && (
+              <CodeMirror
+                value={html}
+                onBeforeChange={handleHtmlChange}
+                className='code'
+                options={{
+                  lineWrapping: true,
+                  lint: true,
+                  mode: "css",
+                  lineNumbers: true,
+                  theme: "material",
+                }}
+              />
+            )}
+            {selectedTab === "css" && (
+              <CodeMirror
+                value={css}
+                onBeforeChange={handleCssChange}
+                className='code'
+                options={{
+                  lineWrapping: true,
+                  lint: true,
+                  mode: "css",
+                  lineNumbers: true,
+                  theme: "material",
+                }}
+              />
+            )}
+            <div className='code-bottom'>Edit CSS to see changes</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
